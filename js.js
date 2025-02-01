bb7d46c59a235b1370b9869c1731fa604259da24
muriisantuss <muriisantuss@gmail.com>
2025-01-31 23:26:46 -0300

    :sparkles: feat: test javascript syntax

---
+++ js.js
@@ -0,0 +1,20 @@
+const players = {
+  portuguesePlayer: "Cristino Ronaldo",
+  brazilianPlayer: "Neymar JR",
+  argentineanPlayer: "Messi",
+  
+}
+
+console.log("Football players: ")
+for (ball in players) {
+  console.log("-", players[ball])
+}
+console.log()
+
+const singer = ["Ariana Grande", "Justin Bieber", "SZA"]
+
+console.log("Famous singers: ")
+for (sing of singer) {
+  console.log("-", sing)
+}
+console.log()
