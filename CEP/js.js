5470fcdee37a050125abf9db252048c27953eb15
muriisantuss <muriisantuss@gmail.com>
2025-02-05 22:24:10 -0300

    :rocket: feat: substituição de XMLHttpRequest por fetch()

--- CEP/js.js
+++ CEP/js.js
@@ -1,10 +1,10 @@
 const ajax = new XMLHttpRequest()
 
 $(document).ready(function () {
-  $('#setCep').mask('00000-000');
+  $('#inputCep').mask('00000-000');
 });
 
-const typedCep = document.querySelector(`#setCep`)
+const inputCep = document.querySelector(`#inputCep`)
 const btnSendCep = document.querySelector(`#btnSendCep`)
 const body = document.body
 const header = document.querySelector("#header")
@@ -28,76 +28,59 @@ function resetUI() {
 
 btnReset.addEventListener('click', resetUI);
 
-
-typedCep.addEventListener("keydown", () => {
-  typedCep.classList.add("animate-scale");
-
-  setTimeout(() => {
-    typedCep.classList.remove("animate-scale");
-  }, 300);
+inputCep.addEventListener("keydown", () => {
+  inputCep.classList.add("animate-scale");
+  setTimeout(() => inputCep.classList.remove("animate-scale"), 300);
 });
 
-typedCep.addEventListener("focus", () => {
-  document.querySelector("body").style.backgroundColor = "#272727";
-  document.querySelector("body").style.color = "#b5b5b5";
-  document.querySelector("#textDown").classList.add("hidden")
-  typedCep.classList.add("animate-scale");
-  msg.textContent = ""
+inputCep.addEventListener("focus", () => {
+  body.style.backgroundColor = '#272727';
+  body.style.color = '#b5b5b5';
+  textDown.classList.add('hidden');
+  inputCep.classList.add('animate-scale');
+  msg.textContent = '';
 })
 
-typedCep.addEventListener("blur", () => {
-  document.querySelector("body").style.backgroundColor = "";
-  document.querySelector("body").style.color = "";
-  typedCep.classList.remove("animate-scale");
+inputCep.addEventListener("blur", () => {
+  resetUI()
+  inputCep.classList.remove("animate-scale");
 })
 
 btnSendCep.addEventListener("click", () => {
-  if (typedCep.value == "") {
+  if (!inputCep.value) {
     msg.textContent = "Por favor, digite o CEP.";
     return;
   }
 
-  if (typedCep.value.length < 9) {
+  if (inputCep.value.length < 9) {
     msg.textContent = "O CEP deve ter 8 dígitos. Verifique e tente novamente.";
     return;
   }
 
-  let ajax = new XMLHttpRequest();
-  ajax.open('GET', `https://viacep.com.br/ws/${typedCep.value}/json/`);
-  ajax.send();
+  fetch(`https://viacep.com.br/ws/${inputCep.value}/json/`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.erro) {
+        msg.textContent = 'CEP não encontrado!';
+        msg.style.color = 'red';
+        return;
+      }
 
-  // Verifica a resposta da API
-  ajax.onload = function () {
-    let obj = JSON.parse(this.responseText);
+      document.querySelector('#getCep').innerHTML = `
+        <strong>CEP:</strong> ${data.cep} <br>
+        <strong>Logradouro:</strong> ${data.logradouro} <br>
+        <strong>Bairro:</strong> ${data.bairro} <br>
+        <strong>Cidade:</strong> ${data.localidade} <br>
+        <strong>Estado:</strong> ${data.uf} <br>
+      `;
 
-    if (obj.erro) {
-      msg.textContent = "CEP não encontrado!";
-      msg.style.color = "red";
-      return;
-    }
+      header.textContent = 'Parabéns, você o encontrou!';
+      header.style.color = 'green';
+      inputCep.value = '';
+      textDown.classList.add('animate');
+      header.classList.add('animate');
+      textDown.classList.remove('hidden');
+      textUp.classList.add('hidden');
+    })
 
-    // Caso o CEP seja válido
-    let cep = obj.cep;
-    let logradouro = obj.logradouro;
-    let bairro = obj.bairro;
-    let cidade = obj.localidade;
-    let estado = obj.uf;
-
-    document.querySelector('#getCep').innerHTML =
-      `<strong>CEP:</strong> ${cep} <br> 
-       <strong>Logradouro:</strong> ${logradouro} <br> 
-       <strong>Bairro:</strong> ${bairro} <br> 
-       <strong>Cidade:</strong> ${cidade} <br> 
-       <strong>Estado:</strong> ${estado} <br> 
-    `;
-
-    header.textContent = "Parabéns, você o encontrou!";
-    header.style.color = "green";
-    typedCep.value = ""
-    document.querySelector("#textDown").classList.add("animate");
-    header.classList.add("animate");
-    document.querySelector("#textDown").classList.remove("hidden");
-    document.querySelector("#textUp").classList.add("hidden");
-
-  };
 });
